f4d667a81a6c2c52630e1582503ae75694197470
Kanade <iseekana@gmail.com>
2021-03-06 07:12:44 +0900

    test

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
       title: 'Welcome to Flutter',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Welcome to Flutter'),
+          title: Text('Welcome to Flutter!'),
         ),
         body: Center(
           child: Text('Hello World2'),
